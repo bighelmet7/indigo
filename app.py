24b71835756cbd756b535b888b18114d82a67d03
Abner Andino <brain.agam@gmail.com>
2020-05-25 18:09:33 +0200

    TASK: flask entrypoint

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask_api import status
+from flask import render_template
+from jinja2.exceptions import TemplateNotFound
+from indigo.exceptions.validators import JSONValidatorException
+
+from indigo import create_app
+
+app = create_app(config_obj='indigo.config.ProductionConfig')
+
+@app.errorhandler(TemplateNotFound)
+def not_found(error):
+    return {'error': 'Page not found'}, status.HTTP_404_NOT_FOUND
+
+@app.route('/ping/')
+def ping():
+    return 'Pong'
+
+# INFO: Code snippet: http://flask.pocoo.org/snippets/57/
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
